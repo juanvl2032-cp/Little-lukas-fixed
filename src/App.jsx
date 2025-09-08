4e3fca12695e9b3f51198995964757f95c0ca10c
juanvl2032-cp <juanvl2032@gmail.com>
2025-09-08 06:04:28 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -227,7 +227,7 @@ export default function App() {
           src="/shoppic1.png"
           alt="Little Lukas Party Shop banner"
           className="w-full h-44 md:h-60 lg:h-72 object-cover"
-          style={{ objectPosition: '250% 90%' }}   // tweak this to shift focus
+          style={{ objectPosition: '300% 100%' }}   // tweak this to shift focus
   loading="eager"
   decoding="async"
 
